bd29b1845b67761f5b73c74302f5b5cf43e5d4c4
Olin Kirkland <olin.kirkland@its.ny.gov>
2026-07-16 15:59:18 -0400

    Footer work

--- src/components/the-footer.vue
+++ src/components/the-footer.vue
@@ -1,21 +1,29 @@
 <template>
     <footer class="w-full flex justify-center py-10">
-        <img
-            class="h-15 mb-4"
-            src="../assets/images/logo/logo-on-dark.png"
-            alt="Kirkland Press logo"
-        />
-        <p>
-            <a href="mailto:hello@kirkland.press">hello@kirkland.press</a> |
-            <a href="">https://olinkirk.land</a>
-        </p>
+        <div class="max-w-page flex justify-between gap-3">
+            <div class="max-w-md flex flex-col gap-3 items-start">
+                <img
+                    class="h-15 mb-4"
+                    src="../assets/images/logo/logo-on-light.png"
+                    alt="Kirkland Press logo"
+                />
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet corporis sequi
+                    tempora voluptatem repellat libero veniam modi obcaecati ad eius.
+                </p>
+                <p>
+                    <a href="">https://www.olinkirk.land</a> |
+                    <a href="mailto:hello@kirkland.press">hello@kirkland.press</a>
+                </p>
+            </div>
+        </div>
     </footer>
 </template>
 
 <style lang="scss" scoped>
 footer {
-    background-color: $black;
-    color: $white;
+    background-color: $pale-gray;
+    color: $black;
     > * {
         opacity: 0.75;
     }
