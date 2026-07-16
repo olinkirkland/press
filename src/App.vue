cd0b3dadfa00553a569b60fcc41f93a4be9a477d
Olin Kirkland <me@olinkirk.land>
2026-07-15 22:47:07 -0400

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@
     <main>
         <TheHero />
     </main>
+    <TheFooter />
 </template>
 
 <script setup lang="ts">
 import TheHeader from './components/the-header.vue';
 import TheHero from './components/the-hero.vue';
+import TheFooter from './components/the-footer.vue';
 </script>
 
 <style lang="scss">
