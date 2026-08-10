65b72dbfca8cd7fca2f6ded709fb582effcf0f35
Olin Kirkland <me@olinkirk.land>
2026-08-10 10:07:21 -0400

    WIP

--- scripts/fetch-itch-products.mjs
+++ scripts/fetch-itch-products.mjs
@@ -1,3 +1,6 @@
-// Gets game data of all games I've posted
-// Use Itch Javascript API Itch.getGameData(options)
-// https://
+// The itch.io server-side API lets you query information about your games and account by making HTTP requests to the API hosted at api.itch.io.
+
+// Use Itch Javascript API Itch.getGameData(options) to get an array of game data objects for the games I have published on itch.io.
+// Each game data object contains information about the game, such as its title, description, and download links.
+
+// API key: env.ITCH_API_KEY
